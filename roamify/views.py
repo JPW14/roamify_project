6283ce69818f4d9d2eaf19fb471c9d42308a397e
John Wallace <2725481w@student.gla.ac.uk>
2026-03-06 18:59:21 +0000

    Added static file folder and initial index template

--- roamify/views.py
+++ roamify/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Home Page")
+    return render(request, 'roamify/index.html',)
 
