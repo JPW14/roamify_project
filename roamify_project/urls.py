18494416793ac4a64eced23ccfdae7ba38cc2981
John Wallace <2725481w@student.gla.ac.uk>
2026-03-06 19:12:49 +0000

    Added media folder and updated settings for a dynamic path

--- roamify_project/urls.py
+++ roamify_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from roamify import views
 
 urlpatterns = [
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('', include('roamify.urls')),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
