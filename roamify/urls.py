b2219cc98e4291b6b0789e7711fccdc474ae843b
John Wallace <2725481w@student.gla.ac.uk>
2026-03-24 15:43:44 +0000

    Added destination & post views

--- roamify/urls.py
+++ roamify/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from roamify import views
+
 app_name = 'roamify'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/', views.post, name='post'),
+    path('destination/', views.destination, name='destination')
 ]
--- roamify/views.py
+++ roamify/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'roamify/index.html',)
 
+def post(request):
+    return render(request, 'roamify/post.html')
+
+def destination(request):
+    return render(request, 'roamify/destination.html')
