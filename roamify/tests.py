f58894888e1538962a6263b9d49286325ea394b8
douyeoseke <133113123+douyeoseke@users.noreply.github.com>
2026-03-26 23:11:14 +0000

    Import reverse for URL testing in tests.py

--- roamify/tests.py
+++ roamify/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
 from .models import Profile, Place, Review
 
 class UserProfileTest(TestCase):
